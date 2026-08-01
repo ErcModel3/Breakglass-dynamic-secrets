dd875f489256c42042ec6a627262a702782230d2
Alexander Kirk <alexanderjoshua21@protonmail.com>
2026-08-01 12:20:40 +0100

    Got syslog parser working for Junos over udp

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ class SyslogHandler(socketserver.BaseRequestHandler):
         print("{} sent:".format(self.client_address[0]))
         print(self.data)
 
-with socketserver.UDPServer(("10.0.0.91", 1515), SyslogHandler) as server:
+class UdpSyslogHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        data = self.request[0].strip()
+        device_ip, device_port = self.client_address
+        print(f'{device_ip}:{device_port} sent: {data.decode(errors="replace")}')
+
+# For tcp server
+# with socketserver.TCPServer(("10.0.0.91", 1515), SyslogHandler) as server:
+#     print("... listening for Syslog messages ...")
+#     server.serve_forever()
+
+# For udp server
+with socketserver.UDPServer(("10.0.0.91", 1515), UdpSyslogHandler) as server:
     print("... listening for Syslog messages ...")
     server.serve_forever()
